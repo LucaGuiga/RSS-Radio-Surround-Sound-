2922e9c527188cc9d2c93d2c21b10c61f8bf7e8b
chloerine <jazores@ucsc.edu>
2026-08-29 17:05:07 -0700

    added i2c init wrappers for eaiser use

--- sample_project/main/master_i2c.c
+++ sample_project/main/master_i2c.c
@@ -10,18 +10,28 @@
 #define xm125_address 0x52 // this is the slave (xm-125) address
 #define scl_frequency 100000 //replace this with what it needs to be
 #define i2c_port_0 I2C_NUM_0
+// i2c_master_bus_config_t i2c_mst_config = { 
+//         .clk_source = I2C_CLK_SRC_DEFAULT,
+//         .i2c_port = i2c_port_0,
+//         .scl_io_num = i2c_scl_pin,
+//         .sda_io_num = i2c_sda_pin,
+//         .glitch_ignore_cnt = glitch_ignore_count,
+//         .flags.enable_internal_pullup = true,
+//     };
 
-
-
-// configuration function
-i2c_master_bus_config_t i2c_mst_config = { 
-    .clk_source = I2C_CLK_SRC_DEFAULT,
-    .i2c_port = i2c_port_0,
-    .scl_io_num = i2c_scl_pin,
-    .sda_io_num = i2c_sda_pin,
-    .glitch_ignore_cnt = glitch_ignore_count,
-    .flags.enable_internal_pullup = true,
-};
+esp_err_t i2c_master_init(i2c_clock_source_t clk_src, i2c_port_num_t  i2c_port,int scl_pin_num, int sda_pin_num, uint8_t glitch_ignore_cnt, bool internal_pullup,i2c_master_bus_handle_t* bus_handle){
+    // configuration function
+    i2c_master_bus_config_t i2c_mst_config = { 
+        .clk_source = clk_src,
+        .i2c_port = i2c_port,
+        .scl_io_num = scl_pin_num,
+        .sda_io_num = sda_pin_num,
+        .glitch_ignore_cnt = glitch_ignore_cnt,
+        .flags.enable_internal_pullup = internal_pullup,
+    };
+    return i2c_new_master_bus(&i2c_mst_config, bus_handle);
+}
+    
 /* 
 //should be in the main to throw an error 
 i2c_master_bus_handle_t bus_handle;
@@ -29,11 +39,25 @@ ESP_ERROR_CHECK(i2c_new_master_bus(&i2c_mst_config, &bus_handle));
 */
 
 // target device 
-i2c_device_config_t dev_cfg = {
-    .dev_addr_length = I2C_ADDR_BIT_LEN_7, //seems to be right
-    .device_address = xm125_address,
-    .scl_speed_hz = scl_frequency,
-};
+// i2c_device_config_t dev_cfg = {
+//     .dev_addr_length = I2C_ADDR_BIT_LEN_7, //seems to be right
+//     .device_address = xm125_address,
+//     .scl_speed_hz = scl_frequency,
+// };
+
+// wrapper for i2c_device_config
+// initializes the target for the i2c bus
+// returns esp_ok if it was inited properly, otherwise error code
+// for target_addr_legnth use the espressif i2c length constants
+esp_err_t i2c_target_init(i2c_addr_bit_len_t  target_addr_length, uint16_t target_address,uint32_t scl_freq,i2c_master_dev_handle_t* dev_handle ,i2c_master_bus_handle_t bus_handle ){
+    i2c_device_config_t dev_cfg = {
+    .dev_addr_length = target_addr_length, //seems to be right
+    .device_address = target_address,
+    .scl_speed_hz = scl_freq,
+    };
+    return i2c_master_bus_add_device(bus_handle, &dev_cfg, dev_handle);
+}
+
 
 /* 
 //should be in the main to throw an error 
@@ -63,19 +87,29 @@ void app_main(void)
 {
     printf("Starting I2C master test bench\n");
     printf("initializing i2c master bus\n");
+
+
     i2c_master_bus_handle_t bus_handle;
-    ESP_ERROR_CHECK(i2c_new_master_bus(&i2c_mst_config, &bus_handle));
+    ESP_ERROR_CHECK(i2c_master_init(
+        I2C_CLK_SRC_DEFAULT,
+        i2c_port_0,
+        i2c_scl_pin,
+        i2c_sda_pin,
+        glitch_ignore_count,
+        true,
+        &bus_handle));
     printf("adding slave device\n");
+
     i2c_master_dev_handle_t dev_handle;
-    ESP_ERROR_CHECK(i2c_master_bus_add_device(bus_handle, &dev_cfg, &dev_handle));
+    ESP_ERROR_CHECK(i2c_target_init(I2C_ADDR_BIT_LEN_7,xm125_address,scl_frequency,&dev_handle,bus_handle));
     printf("inited and added a slave device\n");
 
     printf("trying to read/write data");
 
-    printf("starting read/write sequence\n"); //with no device it shouldnt say anything after this or it should throw an error
-    uint8_t reg_address[2] = {0x00,0x20} ;// the specifi register of the .device you want to talk to 
-    uint8_t recieved_data[4]; // a home for the data you recieved 
-    ESP_ERROR_CHECK(i2c_master_transmit_receive(dev_handle, reg_address, sizeof(reg_address), recieved_data, 4, 1000));
+    // printf("starting read/write sequence\n"); //with no device it shouldnt say anything after this or it should throw an error
+    // uint8_t reg_address[2] = {0x00,0x20} ;// the specifi register of the .device you want to talk to 
+    // uint8_t recieved_data[4]; // a home for the data you recieved 
+    // ESP_ERROR_CHECK(i2c_master_transmit_receive(dev_handle, reg_address, sizeof(reg_address), recieved_data, 4, 1000));
 }
 
 #endif
--- sample_project/main/master_i2c.h
+++ sample_project/main/master_i2c.h
@@ -1,14 +1,26 @@
 // this file is the header file for the master i2c comfiguration
 // this is specifically for communicating with the xm-125 over I2C
 
-#ifndef MASTER_I2C
-#define MASTER_I2C
+#ifndef MASTER_I2C_H
+#define MASTER_I2C_H
+
+#include <stdint.h>
+#include <stdbool.h>
+#include "driver/i2c_master.h"
+#include "esp_err.h"
 #define i2c_scl_pin 22
 #define i2c_sda_pin 21
 
+// a wrapper for i2c_master_bus_config
+// initializes the i2c bus
+// returns esp_ok if it was inited properly, otherwise error code
+esp_err_t i2c_master_init(i2c_clock_source_t clk_src,i2c_port_num_t  i2c_port,int scl_pin_num, int sda_pin_num, uint8_t glitch_ignore_cnt, bool internal_pullup,i2c_master_bus_handle_t* bus_handle); 
 
-extern i2c_master_bus_config_t i2c_mst_config;
-extern i2c_device_config_t dev_cfg;
 
+// wrapper for i2c_device_config
+// initializes the target for the i2c bus
+// returns esp_ok if it was inited properly, otherwise error code
+// for target_addr_legnth use the espressif i2c length constants
+esp_err_t i2c_target_init(i2c_addr_bit_len_t  target_addr_length, uint16_t target_address,uint32_t scl_freq,i2c_master_dev_handle_t* dev_handle ,i2c_master_bus_handle_t bus_handle );
 
 #endif 
